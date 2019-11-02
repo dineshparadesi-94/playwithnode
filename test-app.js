4454a8655c955f24c29c528211d11eba22b1a837
Dinesh Paradesi <zeus.in.da.cloud@gmail.com>
2019-11-02 23:21:38 +1000

    objects

--- test-app.js
+++ test-app.js
@@ -15,4 +15,23 @@ const intro = (usrname,usrage,usrhobbies) => {
 const add = (a,b) => a+b ;
 console.log(add(1,2));
 
+const addone = a => a+1;
+console.log(addone(2));
+
+const addrand = () => 2+3;
+console.log(addrand());
+
 console.log(intro(name,age,hobbies));
+
+// A key-value pair is also called a "property" or a "field" of the object.
+//creating objects
+
+const person ={
+  name:'dinesh',
+  age:24,
+  greet: function() {
+    console.log('My name is ' + this.name);
+  }
+};
+console.log(person);
+person.greet();
