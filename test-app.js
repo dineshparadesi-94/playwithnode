8856e6ca3420e0e054859fa9086dc8ad3a1663b1
Dinesh Paradesi <zeus.in.da.cloud@gmail.com>
2019-11-02 22:57:02 +1000

    synatx change for a function

--- test-app.js
+++ test-app.js
@@ -3,9 +3,9 @@ console.log("Hello There, I initialized Git");
 let name = "Maisha";
 let age = "20";
 let hobbies = "Video Production";
+age = 21;
 
-
-function intro(usrname,usrage,usrhobbies) {
+const intro = (usrname,usrage,usrhobbies) => {
   return (
     'My name is, ' + usrname +
      ' my age is ' + usrage +
