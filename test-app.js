538a26edbe02c6be41eb817b8e7be8732543390f
Dinesh Paradesi <zeus.in.da.cloud@gmail.com>
2019-11-03 14:05:52 +1000

    object destructuring

--- test-app.js
+++ test-app.js
@@ -1,27 +1,27 @@
 console.log("Hello There, I initialized Git");
 
-let name = "Maisha";
-let age = "20";
-let hobbies = "Video Production";
-age = 21;
+// let name = "Maisha";
+// let age = "20";
+// let hobbies = "Video Production";
+// age = 21;
 
-const intro = (usrname,usrage,usrhobbies) => {
-  return (
-    'My name is, ' + usrname +
-     ' my age is ' + usrage +
-    ' and my hobby is ' + usrhobbies
-  );
-}
-const add = (a,b) => a+b ;
-console.log(add(1,2));
-
-const addone = a => a+1;
-console.log(addone(2));
-
-const addrand = () => 2+3;
-console.log(addrand());
-
-console.log(intro(name,age,hobbies));
+// const intro = (usrname,usrage,usrhobbies) => {
+//   return (
+//     'My name is, ' + usrname +
+//      ' my age is ' + usrage +
+//     ' and my hobby is ' + usrhobbies
+//   );
+// }
+// const add = (a,b) => a+b ;
+// console.log(add(1,2));
+//
+// const addone = a => a+1;
+// console.log(addone(2));
+//
+// const addrand = () => 2+3;
+// console.log(addrand());
+//
+// console.log(intro(name,age,hobbies));
 
 // A key-value pair is also called a "property" or a "field" of the object.
 //creating objects
@@ -76,8 +76,10 @@ console.log(toarrayn(1,3,2,5));
 /////////////////////////////////////////////////////////////////////////////////
 //destructuring
 
-const printname = (persondata) => {
-  console.log(persondata.name);
+const printname = ({ name }) => {
+  console.log(name);
 }
 printname(person);
 //object destructuring
+const { name, age } = person;
+console.log(name,age);
