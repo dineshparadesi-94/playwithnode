e6ccccfc7052fc4580bdc9e6b3a2afa0ed40acd5
Dinesh Paradesi <zeus.in.da.cloud@gmail.com>
2019-11-04 11:30:54 +1000

    Response

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req,res) => {
-console.log(req.url, req.method, req.headers);; //understanding requests
+//console.log(req.url, req.method, req.headers);; //understanding requests
 //process.exit();
+
+res.setHeader('Content-Type','text/html');
+res.write('<html>');
+res.write('<head><title>Response Page</title>');
+res.write('<body>Hello there from Nodejs Server!</body></head>');
+res.write('</html>');
+res.end();
 });
 
 server.listen(5100);
