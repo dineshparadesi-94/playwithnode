cb86ac57cbb032147feaae0e3b210efdd4878052
Dinesh Paradesi <zeus.in.da.cloud@gmail.com>
2019-11-04 11:19:10 +1000

    //understanding requests

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((req,res) => {
-console.log(req);;
-process.exit();
+console.log(req.url, req.method, req.headers);; //understanding requests
+//process.exit();
 });
 
 server.listen(5100);
