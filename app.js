b0269b795c2d218698f4cf36a8730f68af0d1a9d
Dinesh Paradesi <zeus.in.da.cloud@gmail.com>
2019-11-04 11:48:13 +1000

    request -> input ->response

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const http = require('http');
-
+const fs = require('fs'); //file system
 const server = http.createServer((req,res) => {
 //console.log(req.url, req.method, req.headers);; //understanding requests
 //process.exit();
+
 const url =req.url;
+const method = req.method; // importing methods
 if(url === '/'){
   res.write('<html>');
   res.write('<head><title>Welcome Page</title>');
   res.write('<body><form action = "/message" method="POST"><input type="text" name=message><button type="submit">Submit</button></form></body></head>');
   res.write('</html>');
   return res.end();   //after res.end -> res.write should not be used so return :)
+}
+if(url === '/message' && method === "POST"){
+fs.writeFileSync('message.txt','Hi There');
+res.statusCode = 302; // The status code is for redirectering url
+res.setHeader('Location','/');
+return res.end();
 }
 res.setHeader('Content-Type','text/html');
 res.write('<html>');
