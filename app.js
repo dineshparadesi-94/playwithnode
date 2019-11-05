46231115bc70aa026ba36566b24ae7b307a57260
Dinesh Paradesi <zeus.in.da.cloud@gmail.com>
2019-11-05 11:31:40 +1000

    export methonds

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
 const routes = require('./routes');//custom
+console.log('rotues.someText');
 
 const server = http.createServer(routes.handler);
 //console.log(req.url, req.method, req.headers);; //understanding requests
