b48bc7c64b8a3bff6c566748a8e3dcfbae60d659
Dinesh Paradesi <zeus.in.da.cloud@gmail.com>
2019-11-05 11:17:10 +1000

    moving the routing to routes.js

--- app.js
+++ app.js
@@ -1,43 +1,9 @@
 const http = require('http');
-const fs = require('fs'); //file system
-const server = http.createServer((req,res) => {
+
+const server = http.createServer((req,res) => {});
 //console.log(req.url, req.method, req.headers);; //understanding requests
 //process.exit();
-const url =req.url;
-const method = req.method; // importing methods
-if(url === '/'){
-  res.write('<html>');
-  res.write('<head><title>Welcome Page</title>');
-  res.write('<body><form action = "/message" method="POST"><input type="text" name=message><button type="submit">Submit</button></form></body></head>');
-  res.write('</html>');
-  return res.end();   //after res.end -> res.write should not be used so return :)
-}
-if(url === '/message' && method === "POST"){
-  const body = [];
-  req.on('data',(chunk)=>{
-    console.log(chunk);
-    body.push(chunk);
-  }); //event listening -> this case data event -> this is fired when a new chunk is ready
-  return req.on("end",()=> {
-    const parsebody = Buffer.concat(body).toString(); // Works with text only
-    console.log(parsebody);
-    const message =parsebody.split('=')[1];
-    fs.writeFile('message.txt',message,err=> {
-      res.statusCode = 302; // The status code is for redirectering url
-      res.setHeader('Location','/');
-      return res.end();
-    });
-    //fs.writeFileSync('message.txt',message);// This is stop the next line untill executed
-
-  });
-
-}
-res.setHeader('Content-Type','text/html');
-res.write('<html>');
-res.write('<head><title>Response Page</title>');
-res.write('<body>Hello there from Nodejs Server!</body></head>');
-res.write('</html>');
-res.end();
-});
+// const url =req.url;
+// const method = req.method; // importing methods
 
 server.listen(5100);
